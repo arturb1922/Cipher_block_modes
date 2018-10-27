a92323dfd2ad3cfe1213ea00b7e11285d883f8c9
Artur Bronka <artur.bronka@student.put.poznan.pl>
2018-10-27 21:05:12 +0200

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,28 @@
-
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        byte[] plain = "abcdefghijklmnopabcdefghijklmnop".getBytes();
+        byte[] key = "klucz--128-bitowklucz--128-bitow".getBytes();
+        String plainString= new String(plain,"UTF-8");
+        String encryptedString;
+        String decryptedStrng;
+        SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+        System.out.println(plainString);
 
-    private static SecretKeySpec skey;
-    private static byte[] key;
+        // szyfrowanie
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte encrypted[] = cipher.doFinal(plain);
+        encryptedString=new String (encrypted,"UTF-8");
+        System.out.println(encryptedString);
 
-    public static void setSkey(String tempKey) {
-        MessageDigest hash = null;
-
-        try{
-            key=tempKey.getBytes("UTF-8");
-            hash=MessageDigest.getInstance("SHA-256");
-            key=hash.digest(key);
-            key= Arrays.copyOf(key,16);
-            skey= new SecretKeySpec(key,"AES");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String encode(String plainText, SecretKeySpec skey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE,skey);
-        return null;
-    }
-
-//comment
-
-    public static void main(String[] args) {
+        // deszyfrowanie
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte decrypted[] = cipher.doFinal(encrypted);
+        decryptedStrng=new String (decrypted,"UTF-8");
+        System.out.println(decryptedStrng);
 
     }
 }
